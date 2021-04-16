87e5f7be53f86498c80f05068ecc4f89e12e0d53
Julia-K <jkozlowska5656@gmail.com>
2021-04-16 14:47:22 +0200

    Create user service

---
+++ src/main/java/kjm/linkverifier/auth/service/CurrentUser.java
@@ -0,0 +1,35 @@
+package kjm.linkverifier.auth.service;
+
+import kjm.linkverifier.auth.models.User;
+import kjm.linkverifier.auth.repository.UserRepository;
+import kjm.linkverifier.auth.security.jwtToken.AuthTokenFilter;
+import kjm.linkverifier.auth.security.jwtToken.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class CurrentUser {
+
+    private static UserRepository userRepository;
+
+    private static AuthTokenFilter tokenAuthenticationFilter;
+
+    private static JwtUtils jwtUtils;
+
+    public CurrentUser(UserRepository userRepository,
+                       AuthTokenFilter tokenAuthenticationFilter,
+                       JwtUtils jwtUtils) {
+        CurrentUser.userRepository = userRepository;
+        CurrentUser.jwtUtils = jwtUtils;
+        CurrentUser.tokenAuthenticationFilter = tokenAuthenticationFilter;
+    }
+
+    public static User getCurrentUser(HttpServletRequest request){
+        String token = tokenAuthenticationFilter.parseJwt(request);
+        return userRepository.findByEmail(jwtUtils.getUserEmailJwtToken(token)).orElseThrow(() ->
+                new RuntimeException("Error: User with email: " + jwtUtils.getUserEmailJwtToken(token) + " is not authenticated"));
+    }
+}
